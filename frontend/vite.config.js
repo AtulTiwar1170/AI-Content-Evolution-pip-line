a1e3ca6118c5735c81b118b5fc02fe5096ee1ad2
AtulTiwar1170 <atult2620@gmail.com>
2025-12-25 15:29:04 +0530

    allowing render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    host: true,
+    port: 5173,
+    // This section allows Render to access the Vite dev server
+    allowedHosts: [
+      'content-evolution.onrender.com',
+      '.onrender.com' // This allows any sub-domain on render
+    ]
+  }
 })
